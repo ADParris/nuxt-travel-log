f4ffb810067230df7cdee1f89d9e7145168128d1
Andrew Parris <andrew@adparris.com>
2026-07-23 19:21:37 -0700

    :sparkles: setup tailwindcss / daisy ui / icons (#2)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
